5c9595256143656a7f059b110fe79bd8bfa3c6b0
xuxiong <18720920393@163.com>
2020-05-06 17:53:32 +0800

    1、redis分布式锁添加备注

--- src/main/java/com/gzcstec/service/RedisLockService.java
+++ src/main/java/com/gzcstec/service/RedisLockService.java
@@ -19,18 +19,23 @@ public class RedisLockService {
 
     /**
      * 加锁
+     * @param key 唯一键
+     * @param value 超时时间
      * @return
      */
     public boolean lock(String key , String value) {
+        //1、使用redis中的setnx方法，如果设置失败，说明已经有该key加锁了
         if(stringRedisTemplate.opsForValue().setIfAbsent(key , value)) {
             //加锁成功
             return true;
         }
-
+        //获取key原来的加锁值
         String currentTime = stringRedisTemplate.opsForValue().get(key);
+        //2、判断是否有锁是否过期，如果过期则获得锁，否则后续逻辑无法操作
         if(!StringUtils.isEmpty(currentTime) && Long.parseLong(currentTime) < System.currentTimeMillis()) {
-            //查看是否先获取钥匙
+            //3、使用redis中的getset方法，获取原来锁值和保存新的锁值
             String oldTime = stringRedisTemplate.opsForValue().getAndSet(key , value);
+            //4、防止加锁过程中并发问题，使用比较原锁值，相等则对应的逻辑处理加锁了，否则并发中的其他请求没有抢到锁
             if(!StringUtils.isEmpty(oldTime) && oldTime.equals(currentTime)) {
                 return true;
             }
@@ -45,7 +50,9 @@ public class RedisLockService {
     public void unlock(String key , String value) {
         //安全解锁
         try {
+            //1、根据Key获取原锁值
             String currentTime = stringRedisTemplate.opsForValue().get(key);
+            //2、根据锁值相等来解锁
             if(!StringUtils.isEmpty(currentTime) && currentTime.equals(value)) {
                 stringRedisTemplate.opsForValue().getOperations().delete(key);
             }
